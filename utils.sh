dcfe9a093edd8006d44c7a29da90a27422af874a
Christopher Cordero <ccordero@protonmail.com>
2019-09-25 21:37:29 -0400

    negate conditional

---
+++ utils.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+function capture_stdout_and_stderr_if_successful {
+    set +e
+    COMMAND=$*
+    printf "Running %s ... " "${COMMAND}"
+
+    OUTPUT=$($COMMAND 2>&1)
+    if [[ ! $? ]]; then
+        export AT_LEAST_ONE_ERROR=1
+        printf "%bFailed%b\n" "${RED}" "${GREEN}"
+        printf "%s\n\n" "${OUTPUT}"
+    else
+        printf "%bSuccess!%b\n" "${GREEN}" "${RESET}"
+    fi
+    set -e
+}
+
+function store_if_at_least_one_error {
+    set +e
+    "$@"
+    [[ ! $? ]] && export AT_LEAST_ONE_ERROR=1
+    set -e
+}
+
+function prompt() {
+    echo "$@" >&1
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) break;;
+            No ) exit 1;;
+        esac
+    done
+}
